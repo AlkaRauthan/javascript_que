eb8e21dc53df97fafb4b43bfb1a88b511f81806a
Alka Rauthan <rauthanalka@gmail.com>
2021-08-17 14:41:56 +0530

    Update sumWithcurrying.js

--- sumWithcurrying.js
+++ sumWithcurrying.js
@@ -15,4 +15,29 @@ function sum(x){
       }
     }
 }
-sum(1)(2)(3)
+
+var r = sum(1)(2)(3);
+console.log(r);
+
+function add(...args1){
+  return function(...args2){
+    return function(...args3){
+      return args1.concat(args2).concat(args3).reduce((a,b)=>a+b)}}}
+
+console.log(add(1)(2)(3))
+console.log(add(1,2)(3,4)(5))
+
+function curry(fn){
+  return function(a){
+    return function(b){
+      return fn(a,b)
+    }
+  }
+}
+
+function addition(a,b){
+  return a +b ;
+}
+let curriedSum = curry(addition);
+let result = curriedSum(1)(2)
+console.log(result);

ac7c5dbb01ae3d10da1b1f1d8ed0d48c548f3d6b
Alka Rauthan <rauthanalka@gmail.com>
2024-05-29 10:27:53 +0530

    Update customBind.js

--- customBind.js
+++ customBind.js
@@ -1,4 +1,12 @@
 
+function sayHello(){
+  return "Hello " + this.name;
+}
+        
+var obj = {name: "Sandy"};
+        
+sayHello.call(obj);
+
 const info = {
   firstName: "Alka",
   lastName:"Rauthan"

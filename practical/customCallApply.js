02d078ff7933cf01d3feaa7822232e85fd4ccfa3
Alka Rauthan <rauthanalka@gmail.com>
2024-05-31 13:55:28 +0530

    Create customCallApply.js

---
+++ practical/customCallApply.js
@@ -0,0 +1,30 @@
+
+const objIntro ={
+name:"rahul",
+city:"gwalior"}
+
+function sayIntro (company){
+console.log(`name is ${this.name}, place is ${this.city} and company is ${company}`)
+}
+  
+Function.prototype.myCall= function (context,...args){
+  if(typeof this !=="function"){
+    throw new Error(this,"invalid call")
+  }
+  context.fnc = this;
+  context.fnc(...args);
+}
+
+sayIntro.myCall(objIntro,"cognizant")
+
+Function.prototype.myApply= function (context,args){
+  if(typeof this !=="function"){
+  throw new Error(this,"invalid call")
+  }
+  if(!Array.isArray(args)){
+    throw new TypeError("arguments are not in array")
+  }
+  context.fnc = this;
+  context.fnc(...args);
+}
+sayIntro.myApply(objIntro,["cognizant","gurgaon"])
